ec5ce2fb07bfb9d90c83042049e665419eb5b24d
sslinux <guiyin.xiong@gmail.com>
2016-10-19 19:01:44 +0800

    pexpect_module

---
+++ codes/random_module.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+import random
+checkcode = ''
+for i in range(4):
+    current = random.randrange(0,4)
+    if current != i:
+        temp = chr(random.randint(65,90))
+    else:
+        temp = random.randint(0,9)
+    checkcode += str(temp)
+
+print(checkcode)
+
